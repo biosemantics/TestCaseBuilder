caae67a9af99df1a7e226dab21574cd50e20e2a1
elviscat <elviscat@gmial.com>
2013-12-05 16:38:04 -0700

    first commit

---
+++ src/main/java/edu/arizona/sirls/testcasebuilder/Main.java
@@ -0,0 +1,14 @@
+package edu.arizona.sirls.testcasebuilder;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Elvis!!");
+	}
+
+}
